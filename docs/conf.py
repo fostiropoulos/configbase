03d5865a96aa66997202c5414ee23c33728db3df
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2024-01-14 20:18:59 +0000

    black

--- docs/conf.py
+++ docs/conf.py
@@ -6,21 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ConfigBase'
-copyright = '2024, Iordanis Fostiropoulos'
-author = 'Iordanis Fostiropoulos'
+project = "ConfigBase"
+copyright = "2024, Iordanis Fostiropoulos"
+author = "Iordanis Fostiropoulos"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
